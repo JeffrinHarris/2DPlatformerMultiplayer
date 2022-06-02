fcd848577b9adec1e23ef52a57d46cadddb95d9e
SubsTheTechnomancer <54163143+SubsTheTechnomancer@users.noreply.github.com>
2022-06-02 18:23:48 +0530

    Parallax done
    I didn't steal the code like a useless mugup jeff

---
+++ Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScript : MonoBehaviour
+{
+
+    public float amount = 0f;
+    public GameObject cam;
+
+    private Vector3 currentPos;
+    private Vector3 camPos;
+    private float width;
+
+    void Start()
+    {
+        currentPos = GetComponent<Transform>().position;
+        camPos = cam.GetComponent<Transform>().position;
+        width = GetComponent<SpriteRenderer>().bounds.size.x/2f;
+    }
+
+    void Update()
+    {
+        float posDiff = cam.GetComponent<Transform>().position.x - camPos.x;
+        float newPos = currentPos.x + amount*posDiff;
+
+        float posChange = camPos.x - newPos;
+
+        if(posChange > width)
+            newPos += width*2;
+        
+        if(posChange < -1f*width)
+            newPos -= width*2;
+
+        GetComponent<Transform>().position = new Vector3(newPos,currentPos.y, currentPos.z);
+
+
+
+        currentPos = GetComponent<Transform>().position;
+        camPos = cam.GetComponent<Transform>().position;
+    }
+}

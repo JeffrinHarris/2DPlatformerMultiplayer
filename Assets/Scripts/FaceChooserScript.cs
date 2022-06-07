e404bd40e1bcee6bcb75b7831b94b53a482ac260
SubsTheTechnomancer <54163143+SubsTheTechnomancer@users.noreply.github.com>
2022-06-07 21:53:13 +0530

    File browser plugin and image upload

---
+++ Assets/Scripts/FaceChooserScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using SimpleFileBrowser;
+
+public class FaceChooserScript : MonoBehaviour
+{
+    
+    private RawImage face;
+    [SerializeField]
+    private UISkin skin;
+
+    private void Start()
+    {
+        face = GetComponentInChildren<RawImage>();
+        FileBrowser.SetFilters(true, new FileBrowser.Filter("Images",".jpg",".png"));
+        FileBrowser.SetDefaultFilter(".png");
+        FileBrowser.Skin = skin;
+    }   
+
+    public void ChooseFace()
+    {
+        FileBrowser.ShowLoadDialog((paths) => fileSelected(paths), () => fileCancelled(),FileBrowser.PickMode.FilesAndFolders,false,null,null,"Choose a Face","Select Face");
+    }
+
+    private void fileSelected(string[] paths)
+    {
+        Debug.Log("File selected! "+ FileBrowser.Result[0]);
+
+        byte[] bytes = FileBrowserHelpers.ReadBytesFromFile(FileBrowser.Result[0]);
+
+        Texture2D tex = new Texture2D(2,2);
+        tex.LoadImage(bytes);
+
+        face.texture = tex;
+    }
+
+    private void fileCancelled()
+    {
+        Debug.Log("File failed to select.");
+    }
+
+}
